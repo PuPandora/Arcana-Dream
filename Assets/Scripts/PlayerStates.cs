07138e00d445b1d69d4928c4f35bf4d1b2ba5a04
PuPandora <pandora6722@gmail.com>
2023-12-01 00:46:03 +0900

    (Feat) 캐릭터 스탯 기능 기본 틀

---
+++ Assets/Scripts/PlayerStates.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStates : MonoBehaviour
+{
+    public static int increaseDamage;
+    public static int increaseSpeed;
+    public static int increaseFireDelay;
+
+    public static int moreDamage;
+    public static int moreSpeed;
+    public static int moreFireDelay;
+
+    public static int increaseHealth;
+    public static int moreHealth;
+
+    public static int increaseHealthRegen;
+    public static int moreHealthRegen;
+
+    public static int increaseMoveSpeed;
+    public static int moremoveSpeed;
+
+    public static int increaseLuck;
+    public static int moreLuck;
+}
