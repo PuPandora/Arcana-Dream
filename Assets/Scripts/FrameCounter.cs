9cc2a1f96fcd240ec828884f1092f594c1f1c5e0
PuPandora <pandora6722@gmail.com>
2023-12-04 12:48:58 +0900

    (Feat) 프레임 측정 FrameCounter

---
+++ Assets/Scripts/FrameCounter.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameCounter : MonoBehaviour
+{
+    private float deltaTime;
+
+    [SerializeField]
+    private int size = 25;
+
+    [SerializeField]
+    private Color color = Color.green;
+
+    public bool isShow;
+
+    void Start()
+    {
+        Debug.Log("Frame Counter가 있습니다.\n빌드할 떄 잊지말고 제거하세요.");
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Update()
+    {
+        deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
+
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            isShow = !isShow;
+        }
+    }
+
+    void OnGUI()
+    {
+        if (isShow)
+        {
+            GUIStyle style = new GUIStyle();
+            Rect rect = new Rect(30, 30, Screen.width, Screen.height);
+
+            style.alignment = TextAnchor.UpperLeft;
+            style.fontSize = size;
+            style.normal.textColor = color;
+
+            float ms = deltaTime / 1000f;
+            float fps = 1.0f / deltaTime;
+            string text = $"{fps:0} FPS\n{ms:0.00} ms";
+
+            GUI.Label(rect, text, style);
+        }
+    }
+}
