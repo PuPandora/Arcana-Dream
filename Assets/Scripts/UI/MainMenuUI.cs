32a084d6063663cce421651b7f9250643af3266c
PuPandora <pandora6722@gmail.com>
2023-12-05 13:30:23 +0900

    (Feat) 게임 시작, 세이브 데이터 고르기 트윈

---
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,46 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuUI : MonoBehaviour
+{
+    public DOTweenAnimation[] mainButtonTweens;
+    public DOTweenAnimation[] selectMemoryTweens;
+
+    public void DoPlayMainButtonTweens(string id)
+    {
+        foreach (var tween in mainButtonTweens)
+        {
+            tween.DOPlayById(id);
+        }
+    }
+
+    public void DoCompleteMainButtons()
+    {
+        foreach (var tween in mainButtonTweens)
+        {
+            tween.DOComplete();
+        }
+    }
+
+    public void DoPlaySelectMemories(string id)
+    {
+        foreach (var tween in selectMemoryTweens)
+        {
+            tween.DORestartById(id);
+        }
+    }
+
+    public void SelectedSaveData(int index)
+    {
+        for (int i = 0; i < selectMemoryTweens.Length; i++)
+        {
+            if (i == index) continue;
+
+            selectMemoryTweens[i].DOPlayById("Hide");
+        }
+    }
+}
